d1d0ad1600af64fcb4cd9d5740783fe4eaade58e
duxard <duxard@gmail.com>
2021-01-07 03:21:10 -0500

    step 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>book 1</li>
+          <li>book 2</li>
+          <li>book 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
