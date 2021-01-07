9bf15770b1c4b61fa90398848b9e166b01ab1287
duxard <duxard@gmail.com>
2021-01-07 04:25:19 -0500

    step 2 - add context provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import './index.css';
+
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
-import './index.css';
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
