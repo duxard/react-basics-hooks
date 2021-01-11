ca9c548ee3a398c92b56cc2c13f24bad8a156f09
duxard <duxard@gmail.com>
2021-01-11 03:41:13 -0500

    accessig context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 export default class BookList extends Component {
+  /**
+   * contextType will take value provided by ThemeContext.Provider
+   * and attach it to this (Navbar) component
+   */
+  static contextType = ThemeContext;
   render() {
+
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return (
-      <div className="book-list">
+      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
         <ul>
-          <li>book 1</li>
-          <li>book 2</li>
-          <li>book 3</li>
+          <li style={{ background: theme.ui }}>book 1</li>
+          <li style={{ background: theme.ui }}>book 2</li>
+          <li style={{ background: theme.ui }}>book 3</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
+/**
+ * 1st variant of implementing this functionality - to use ThemeContext.Consumer
+ * and attach it to this (Navbar) component
+ * It has the following pros:
+ * - it can be used in functional components (as opposed to "contextType" method
+ *   which is being used in "BookList" component
+ * - ThemeContext.Consumer can consume multiple contexts
+ */
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          <h1>Context App</h1>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </div>
+      <ThemeContext.Consumer>
+      {
+        (context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div>
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Context App</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            </div>
+          )
+        }
+      }
+      </ThemeContext.Consumer>
     );
   }
 }
+
+// 2nd variant of implementing the same functionality - to use
+// contextType property
+// export default class Navbar extends Component {
+//   /**
+//    * contextType will take value provided by ThemeContext.Provider
+//    * and attach it to this (Navbar) component
+//    */
+//   static contextType = ThemeContext;
+//   render() {
+//
+//     console.log(this.context);
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//
+//     return (
+//       <div>
+//         <nav style={{ background: theme.ui, color: theme.syntax }}>
+//           <h1>Context App</h1>
+//           <ul>
+//             <li>Home</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//           </ul>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
